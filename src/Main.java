5a9c4a8b94ef730305c5e315e97197643c76f20e
jeniknaum5 <zhenyanaumenko20@gmail.com>
2021-02-18 20:38:57 +0200

    NFA completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static int[][] InputData = {{67,12,92,29,2,68,31,2,74,7,18,16,83,77,87,72,73,57,62,25},
+            {33,97,96,18,41,53,26,74,80,93,85,48,5,30,29,59,98,60,62,24},
+            {19,80,41,2,10,80,26,83,89,40,8,23,38,57,93,31,10,20,5,90}};
 
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++){
+            for (int j=0;j<20;j++){
+                System.out.print(InputData[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        NextFitAlgorithm first = new NextFitAlgorithm(InputData);
+        first.printInputData();
+        System.out.println();
+        first.Algorithm();
     }
+
+
 }
--- src/NextFitAlgorithm.java
+++ src/NextFitAlgorithm.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class NextFitAlgorithm {
+
+    ArrayList<Integer> InputData = new ArrayList<>();
+    int NumbOfContainer = 1;
+    int Counter = 0;
+    int NumbOfCompare = 0;
+
+    public NextFitAlgorithm(int[][] InputData) {
+        for (int i = 0; i < 20; i++)
+            this.InputData.add(InputData[0][i]);
+    }
+
+    public void printInputData() {
+        for (Integer integer : InputData)
+            System.out.print(integer + " ");
+    }
+
+    public void Algorithm() {
+        Counter = InputData.get(0);
+        for (int i = 1; i < InputData.size(); i++) {
+            NumbOfCompare++;
+            if (Counter + InputData.get(i) <= 100) {
+                Counter += InputData.get(i);
+            } else {
+                NumbOfContainer++;
+                Counter = InputData.get(i);
+            }
+        }
+        System.out.println("|||NFA|||");
+        System.out.println("Кол-во сравнений: " + NumbOfCompare);
+        System.out.println("Кол-во контейнеров: "+NumbOfContainer);
+        NumbOfContainer =0;
+        NumbOfCompare =0;
+    }
 }
